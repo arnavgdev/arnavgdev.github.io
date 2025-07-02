ef55a9307b01e52513ddaf1aab6797996bebac7a
Arnav Gupta <arnavg363@gmail.com>
2025-07-02 09:52:02 -0400

    header & footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,26 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from "@tailwindcss/vite";
+
+import icon from "astro-icon";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "static",
+
+  // ← important
+  base: "/",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [
+    icon({
+      include: {
+        mdi: ["*"], // This loads all mdi icons
+      },
+    }),
+  ],
+});
